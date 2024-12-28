446ed61cfb3a791d5039f8e40492f59e41a521a0
jangddol <j991005@korea.ac.kr>
2024-12-29 04:18:28 +0900

    fix : fix the unit of volume

--- Flow_and_Temp/RFM/RFMdaemon.py
+++ Flow_and_Temp/RFM/RFMdaemon.py
@@ -11,7 +11,7 @@ from RFMserial import RFMserial
 from channel import Channel, ChannelName, convert_int_to_channel
 from schedularwindow import Action, SchedularWindow, ScheduleWidget
 
-SERIAL_ON = True
+SERIAL_ON = False
 
 # graphic constants
 COLUMNNUM = 3
